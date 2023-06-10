b6755da472e1a40caccdf025569462c9771a865a
ConnorSouls <pman39868@gmail.com>
2023-06-10 17:26:04 -0600

    cambioColorCSSCasas

---
+++ dynamics/php/config.php
@@ -0,0 +1,35 @@
+<?php
+    const HOST="localhost";
+    const USER="danibutnotdani";
+    const PASS="danibutnotdani";
+    const DB="Twitter";
+
+    const RUTA = "../../docs/sql/";
+    const nRespaldo = "Twitter.sql";
+
+
+    function connect(){
+        $conexion = mysqli_connect(HOST,USER,PASS,DB);
+
+        if(!$conexion){
+            echo "Error: No se pudo conectar a MySQL. <br> ";
+            echo "Error: " . mysqli_connect_errno() ."<br>";
+            echo "Error: " . mysqli_connect_error() . "<br>";
+            exit;
+        }
+        return $conexion;
+    }
+    function generarRespaldo()
+    {
+        $command = "mysqldump -u" . USER . " -p" . PASS . " " . DB . " > " . RUTA.nRespaldo;
+
+        system($command, $output);
+
+        if ($output !== 0) {
+            echo 'Ocurrió un error al crear el respaldo de la base de datos.';
+        }
+    }
+    generarRespaldo();
+    //var_dump(connect());
+
+
