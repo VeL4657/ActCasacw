2241704b58abe5a9910571dcda20bc92a0d9775d
Ana12092001 <anauicab.benavides@gmail.com>
2023-06-11 22:59:31 -0600

    pagina principal y css

--- dynamics/php/paginaaaaa.php
+++ dynamics/php/paginaaaaa.php
@@ -1,18 +1,33 @@
-<?php
-    session_start();
-    $_SESSION["casa"] =(isset($_POST['btn']) && $_POST["btn"] != "")? $_POST['btn'] : false;
-    echo  $_SESSION["casa"];
-?>
-
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Obtain casa</title>
-    <script src="../casas.js"></script>
+        <title>Pagina principal</title>
+    	<link rel="stylesheet" href="../../Statics/Styles/paginaaa.css" type="text/css">
 </head>
 <body>
 
+    <h1>BIENVENIDX , USUARIO </h1>
+    <p class="tuspuntos">Tienes un total de : numero puntos!</p>
+    <div class= "cuadrito">
+    <button class="sumar" id='aumentar' onclick="aumentar()">+</button>
+    <button class="restar" id='disminuir' onclick="disminuir()">-</button>
+    <input type='text' id="cantidad" value="0">
+    <button class="enviar" >Enviar</button>
+    </div>
+    <p class= "casa">Perteneces a la casa: casa </p>
+    <p class="puntaje" >Tu casa tiene un total de : numero puntos </p>
+    <?php
+			$dato =$_POST;
+	?>
+<script>
+    var inicio = 0; 
+    function aumentar(){ 
+    var cantidad = document.getElementById('cantidad').value = ++inicio;
+    }
+
+    function disminuir(){ 
+    var cantidad = document.getElementById('cantidad').value = --inicio; 
+    }
+</script>
 </body>
 </html>
